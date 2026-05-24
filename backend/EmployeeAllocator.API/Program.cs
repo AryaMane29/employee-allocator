9b23332fd50cff7545ac7622d3f0ba3b067bcf9c
AryaMane29 <aaryamane2929@gmail.com>
2026-05-24 16:17:37 +0530

    Fix DATABASE_URL parsing

--- backend/EmployeeAllocator.API/Program.cs
+++ backend/EmployeeAllocator.API/Program.cs
@@ -10,12 +10,20 @@ using EmployeeAllocator.API.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Database
-var connectionString = Environment.GetEnvironmentVariable("DATABASE_URL") 
-    ?? builder.Configuration.GetConnectionString("DefaultConnection");
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+string connectionString;
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
-    
+if (databaseUrl != null)
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+    connectionString = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+}
+else
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
+}
+
 // JWT Auth
 var jwtKey = builder.Configuration["Jwt:Key"]!;
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
